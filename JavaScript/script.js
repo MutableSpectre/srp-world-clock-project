51d4cf7a7302b566ea0c3bed3437b894aed8a9d2
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2024-08-31 12:52:19 -0400

    Added Current Time Zone

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -30,6 +30,9 @@ parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("h:mm:ss [<small
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment().tz().guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
