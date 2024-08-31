a8031d0aa8eb7653514d30f183bcc0983f2184c4
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2024-08-31 13:00:48 -0400

    Added 'o' and ',' to dates.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,28 +3,28 @@ function updateTime() {
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML = moment().tz("America/Los_Angeles").format("MMMM D, YYYY");
+losAngelesDateElement.innerHTML = moment().tz("America/Los_Angeles").format("MMMM Do, YYYY");
 losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("h:mm:ss [<small>]A[</small>]");
 
 //Sydney
 let sydneyElement = document.querySelector("#sydney");
 let sydneyDateElement = sydneyElement.querySelector(".date");
 let sydneyTimeElement = sydneyElement.querySelector(".time");
-sydneyDateElement.innerHTML = moment().tz("Australia/Sydney").format("MMMM D, YYYY");
+sydneyDateElement.innerHTML = moment().tz("Australia/Sydney").format("MMMM Do, YYYY");
 sydneyTimeElement.innerHTML = moment().tz("Australia/Sydney").format("h:mm:ss [<small>]A[</small>]");
 
 //Tokyo
 let tokyoElement = document.querySelector("#tokyo");
 let tokyoDateElement = tokyoElement.querySelector(".date");
 let tokyoTimeElement = tokyoElement.querySelector(".time");
-tokyoDateElement.innerHTML = moment().tz("Asia/Tokyo").format("MMMM D, YYYY");
+tokyoDateElement.innerHTML = moment().tz("Asia/Tokyo").format("MMMM Do, YYYY");
 tokyoTimeElement.innerHTML = moment().tz("Asia/Tokyo").format("h:mm:ss [<small>]A[</small>]");
 
 //Paris
 let parisElement = document.querySelector("#paris");
 let parisDateElement = parisElement.querySelector(".date");
 let parisTimeElement = parisElement.querySelector(".time");
-parisDateElement.innerHTML = moment().tz("Europe/Paris").format("MMMM D, YYYY");
+parisDateElement.innerHTML = moment().tz("Europe/Paris").format("MMMM Do, YYYY");
 parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("h:mm:ss [<small>]A[</small>]");
 };
 
@@ -40,7 +40,7 @@ function updateCity(event) {
     <div class="city">
         <div>
             <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
             </div>
             <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
         </div>
